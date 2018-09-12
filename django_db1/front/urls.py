cc1171b59929693805b3a892155eddfb0fc5cd09
dteer <1206099050@qq.com>
2018-09-12 18:21:51 +0800

    数据库学习的开端

---
+++ django_db1/front/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path, include
+from . import views
+# urlpatterns = [
+#     path('/', views.index, name='index'),
+#     path('add_book/',views.add_book, name="add_book"),
+#     path('book_detail/',views.book_detail, name="book_detail")
+# ]
